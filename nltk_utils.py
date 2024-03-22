aa231fd78b3f673c16f7c378ec81b07a4a62799a
Fairuz Elqi Mochammad <elqimochammad12@gmail.com>
2024-03-22 14:55:38 +0700

    add utils token and stem

---
+++ nltk_utils.py
@@ -0,0 +1,21 @@
+import nltk
+#nltk.download('punkt')
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from nltk.stem.porter import PorterStemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+words = ["mencoba","cobalah","coba"]
+
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
+
