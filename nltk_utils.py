e8430738506501373712c54f68fb4ee9150e6c18
Fairuz Elqi Mochammad <elqimochammad12@gmail.com>
2024-04-04 12:31:35 +0700

    add bag of word and dataset

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,7 +1,9 @@
 import nltk
 #nltk.download('punkt')
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from nltk.stem.porter import PorterStemmer
+# from nltk.stem.porter import PorterStemmer
+import numpy as np
+
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
@@ -12,6 +14,18 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w, in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
+
+#sentence = ["hello", "how","are","you","thank","you"]
+#words = ["hi","hello","I","you","bye","thank","cool"]
+#bog = bag_of_words(sentence, words)
+
+#print(bog)
+
 
 
--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -22,4 +26,33 @@ all_words = sorted(set(all_words))
 
 tags = sorted(set(tags))
 
-print(tags)
+X_train = []
+y_train = []
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[idx], self.y_data[idx]
+    
+    def __len__(self):
+        return self.n_samples
+
+#hyper parameter
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
